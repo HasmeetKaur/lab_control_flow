a63da2ee95bc066517bfbf516d8f618478e68beb
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-08-12 09:07:55 +0100

    outline weather

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -5,7 +5,41 @@ public class WhatToWear {
                 int temperature;
                 String weatherDescription;
 
+                if (temperature <= 20) {
+                        if (weatherDescription == "Rainy"){
+                                coldRain();
+                        }
+                        else if (weatherDescription == "Sunny"){
+                                coldSun();
+                        }
+                        else if (weatherDescription == "Cloudy"){
+                                coldCloud();
+                        }
+                }
 
+                if ((temperature > 20) && (temperature <=30)) {
+                        if (weatherDescription == "Rainy"){
+                                warmRain();
+                        }
+                        else if (weatherDescription == "Sunny"){
+                                warmSun();
+                        }
+                        else if (weatherDescription == "Cloudy"){
+                                warmCloud();
+                        }
+                }
+
+                if (temperature >= 30) {
+                        if (weatherDescription == "Rainy"){
+                                hotRain();
+                        }
+                        else if (weatherDescription == "Sunny"){
+                                hotSun();
+                        }
+                        else if (weatherDescription == "Cloudy"){
+                                hotCloud();
+                        }
+                }
                 // input weather details
                 Scanner userInput = new Scanner(System.in);
         }
