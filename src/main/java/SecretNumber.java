a1b234c604b92e286f0db0731bd808f13792e2ad
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-08-12 08:37:10 +0100

    add secret number game

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
 public class SecretNumber {
     public static void main(String[] args){
         // put code in here
-        int secretnumber =10;
+
+        // Welcome message
+        String startMessage;
+        startMessage = "Welcome to Hasmeet's Secret Number Game! Please take a guess of the secret number which is between 0 and 10.";
+        System.out.println(startMessage);
+        // input number
+        Scanner userInput = new Scanner(System.in);
+
+        int secretNumber;
+        // generates a random number
+        secretNumber = (int) (Math.random() * 10);
+        int guessNumber = 0;
+
+        // define the guess number
+        System.out.print("Please enter your guess of the secret number here: ");
+        guessNumber = userInput.nextInt ();
+        System.out.println(guessNumber);
+
+        // check the number guessed and pass a message dependent on it
+        if (guessNumber == secretNumber) {
+            System.out.println("Yey! You successfully guessed the secret number. It was "+secretNumber+"!");
+        } else if (guessNumber < secretNumber){
+            System.out.println("Your guess is lower than the secret number. The secret number was "+secretNumber+". Play again and please remember it will be a new secret number!");
+        } else if (guessNumber > secretNumber){
+            System.out.println("Your guess is higher than the secret number. The secret number was "+secretNumber+". Play again and please remember it will be a new secret number!");
+        }
     }
 }
+
--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,5 +1,11 @@
-public class WhatToWear{
+import java.util.Scanner;   // ADDED
+public class WhatToWear {
         public static void main(String[] args){
-            // put code in here
+                // put code in here
+                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+                System.out.println("Enter username");
+
+                String userName = myObj.nextLine();  // Read user input
+                System.out.println("Username is: " + userName);  // Output user input
         }
-                }
+}
