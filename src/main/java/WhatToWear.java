264617e7d3f0e934bf4f539d96f24717f34ab234
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-08-12 08:55:42 +0100

    define weather inputs

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -2,10 +2,11 @@ import java.util.Scanner;   // ADDED
 public class WhatToWear {
         public static void main(String[] args){
                 // put code in here
-                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-                System.out.println("Enter username");
+                int temperature;
+                String weatherDescription;
 
-                String userName = myObj.nextLine();  // Read user input
-                System.out.println("Username is: " + userName);  // Output user input
+
+                // input weather details
+                Scanner userInput = new Scanner(System.in);
         }
 }
